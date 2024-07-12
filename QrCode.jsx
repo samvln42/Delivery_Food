28103af8ff05b4f0c492ac123c15b269d420f1e7
seonekr <145427113+seonekr@users.noreply.github.com>
2024-07-12 15:45:20 +0700

    1

--- QrCode.jsx
+++ QrCode.jsx
@@ -6,7 +6,7 @@ const QrCode = () => {
   return (
     <div style={{textAlign: "center", width: "100%", marginTop: "15rem"}}>
         {/* <p>My scan QR code</p> */}
-        <QRCodeSVG value="http://54.180.135.221/" />
+        <QRCodeSVG/>
 
     </div>
   )
